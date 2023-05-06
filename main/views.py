e2fe90fc39c165760cd13b60a83b79dc3a132f5c
avalon <asd95027@gmail.com>
2023-05-06 09:10:18 +0800

    完成功能跟邏輯測試

--- main/views.py
+++ main/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # Create your views here.
 def index(request):
     print('Hello django')
-    return HttpResponse ('<h1>Hello django!</h1>')
+    # return HttpResponse ('<h1>Hello django!</h1>')
+    return render(request,'./index.html')
+
+def password(request):
+    length=eval(request.GET.get('length'))
+    input_length=request.GET.get('input-length')
+    #起始密碼
+    password_char='abcdefghijklmnopqrstuvwxyz'
+    # 加上大寫字母
+    if request.GET.get('uppercase') =='on':
+        password_char+=password_char.upper()
+
+    # 加上數字
+    if request.GET.get('number') is not None:
+        password_char+='0123456789'
+
+    #加入符號
+    if request.GET.get('special') is not None:
+        password_char+='~!@#$%^&*=?'
+
+
+    # 判斷長度
+    if input_length!='':
+        length=eval(input_length)
+    # 產生密碼
+    password=''.join([ random.choice(password_char) for i in range(length)])
+
+    print(length,password,password_char)
+    return render(request,'./password.html',{'password':password})
+
+
+
